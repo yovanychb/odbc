552e3c04b8a4e545a48c0571def51500d544db81
root <root@pdc135-2019.quesitos.com>
2019-05-07 02:01:01 -0600

    Intro a realtime

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t asterisk .
-docker run -it --rm --name asterisk -p 5060:5060/udp -p 10000-10010:10000-10010 asterisk
+docker run -it --rm --network host --name asterisk asterisk
